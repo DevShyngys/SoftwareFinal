53733d153c431d8728b4d7ffa52e447a9d3ac652
DevShyngys <chingizbulatov72@gmail.com>
2025-12-16 21:11:33 +0500

    Models are added

---
+++ src/main/java/com/example/softwarefinal/model/Actor.java
@@ -0,0 +1,20 @@
+package com.example.softwarefinal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "actors")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+
+}
---
+++ src/main/java/com/example/softwarefinal/model/Director.java
@@ -0,0 +1,19 @@
+package com.example.softwarefinal.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "directors")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+}
