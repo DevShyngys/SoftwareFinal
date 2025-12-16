78f1afb6618379fc33ede7d76ec2716504dbe7ff
DevShyngys <chingizbulatov72@gmail.com>
2025-12-16 20:20:59 +0500

    Initial commit

---
+++ src/main/java/com/example/softwarefinal/SoftwareFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwarefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softwarefinal/SoftwareFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwarefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
