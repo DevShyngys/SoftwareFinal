a7e32b1d7614b9ea829a92caf7258d2f0fa468d9
DevShyngys <chingizbulatov72@gmail.com>
2025-12-16 22:18:19 +0500

    DTO classes are added

---
+++ src/main/java/com/example/softwarefinal/dto/ActorDTO.java
@@ -0,0 +1,17 @@
+package com.example.softwarefinal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActorDTO {
+    private Long id;
+    private String name;
+    private int age;
+    private List<MovieDTO> movies;
+}
---
+++ src/main/java/com/example/softwarefinal/dto/DirectorDTO.java
@@ -0,0 +1,17 @@
+package com.example.softwarefinal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DirectorDTO {
+    private Long id;
+    private String name;
+    private int age;
+    private List<MovieDTO> movies;
+}
